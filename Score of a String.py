35d7321461b2cd533f7d2fc2621be417df8cd2ae
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-04-15 19:51:18 -0400

    3110. Score of a String.py

---
+++ Score of a String.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/score-of-a-string/
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s) - 1):
+            a = ord(s[i])
+            b = ord(s[i + 1])
+            temp = abs(a - b)
+            ans = ans + temp
+        
+        return ans
